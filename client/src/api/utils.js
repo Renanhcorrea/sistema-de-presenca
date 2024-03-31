0a0fca43d1cf4b210767ba5b0236972be962d4af
mosanches <mah.osanches@gmail.com>
2024-03-31 00:08:09 -0300

    Rollback env api_url

--- client/src/api/utils.js
+++ client/src/api/utils.js
@@ -1,7 +1,5 @@
-require('dotenv').config()
-
 import axios from 'axios';
-const API_URL = process.env.API_URL;
+const API_URL = 'http://15.228.148.167:3000';
 const codProfessor = 'P002';
 
 export const getProfessor = async () => {
